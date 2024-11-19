592f7f7297e5bc6f06658f46cc86f385c79873d4
Leonardo Flores <florees.leo@gmail.com>
2024-11-19 12:51:22 -0600

    feat: :white_check_mark: PART3 phonebook_backend 3.2
    Se implementa la página /info.
    
    La página muestra la hora en que se recibió la solicitud y cuantas entradas hay en la agenda telefónica en el momento de procesar la solicitud.

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -28,6 +28,19 @@ app.get("/", (request, response) => {
   response.send("<h1>Hello Fullstack Open</h1>");
 });
 
+app.get("/info", (request, response) => {
+  const currentDate = new Date();
+
+  response.send(
+    `<div>
+      <p>Phonebook has info for ${persons.length} ${
+      persons.length === 1 ? "person" : "people"
+    }</p>
+    <p>${currentDate}</p>
+    </div>`
+  );
+});
+
 app.get("/api/persons", (request, response) => {
   response.json(persons);
 });

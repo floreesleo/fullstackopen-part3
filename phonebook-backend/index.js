be8cb883581f0780b483ed6e78a9c132ce569574
Leonardo Flores <florees.leo@gmail.com>
2024-11-25 15:16:09 -0600

    feat: ✅ PART3 phonebook_backend 3.8
    Se configura morgan para que muestre los datos enviados en las solicitudes HTTP POST. Esto, creando un nuevo token que proporciona morgan.

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -4,7 +4,13 @@ const morgan = require("morgan");
 const app = express();
 const PORT = 3001;
 
-app.use(morgan("tiny"));
+morgan.token("data", (req, res) => {
+  return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :data")
+);
 app.use(express.json());
 
 let persons = [
